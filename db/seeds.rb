879573074fa5acf1dc026e5ef805de7929fc53ad
Marco Ricci <mr.ricci79@gmail.com>
2022-09-02 09:31:38 +0100

    added more players

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,4 +54,69 @@ player = Player.create!(name: "Rodrygo",
             )
 puts "Created Player id: #{player.id}"
 
+player = Player.create!(name: "Kimmich",
+              club: "Batern Munchen",
+              nationality: "Germany",
+              age: 27,
+              value: 72,
+              role: "Defender"
+            )
+puts "Created Player id: #{player.id}"
+
+player = Player.create!(name: "Hernandez",
+  club: "AC Milan",
+  nationality: "France",
+  age: 24,
+  value: 50,
+  role: "Defender"
+)
+puts "Created Player id: #{player.id}"
+
+player = Player.create!(name: "Tonali",
+  club: "AC Milan",
+  nationality: "Italy",
+  age: 22,
+  value: 45,
+  role: "Midfielder"
+)
+puts "Created Player id: #{player.id}"
+
+player = Player.create!(name: "Lewandowski",
+  club: "Barcelona",
+  nationality: "Poland",
+  age: 34,
+  value: 40,
+  role: "Forward"
+)
+puts "Created Player id: #{player.id}"
+
+player = Player.create!(name: "Salah",
+  club: "Liverpool",
+  nationality: "Egypt",
+  age: 30,
+  value: 81,
+  role: "Forward"
+)
+puts "Created Player id: #{player.id}"
+
+player = Player.create!(name: "Fabinho",
+  club: "Liverpool",
+  nationality: "Brasil",
+  age: 28,
+  value: 54,
+  role: "Midfielder"
+)
+puts "Created Player id: #{player.id}"
+
+player = Player.create!(name: "Haaland",
+  club: "Machester City",
+  nationality: "Norway",
+  age: 22,
+  value: 135,
+  role: "Forward"
+)
+puts "Created Player id: #{player.id}"
+
+
+puts "Created #{Player.count} players"
 puts "Finished!"
